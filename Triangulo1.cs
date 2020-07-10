37f8e1ae5efa4a97952de57026848ed2917c9cd7
Maria360 <68033327+Maria360@users.noreply.github.com>
2020-07-10 11:27:24 -0500

    Add files via upload

---
+++ Triangulo1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triángulos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos
+            Console.Write("Ingrese Y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Inserte Z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            //Hallar t
+            double zPow = Math.Pow(z, 2), yPow = Math.Pow(y, 2);
+            double t = Math.Sqrt(zPow+yPow);
+
+            //Hallar a
+            double a = Math.Asin((y * Math.Sin(Math.PI / 2)) / t);
+            double aGrados = a * 180/Math.PI;
+
+            //Hallar c
+            double c = Math.PI - (Math.PI) / 2 - a;
+            double cGrados = c * 180/Math.PI;
+
+            //Print results
+            Console.WriteLine($"Los resultados son \n a: {aGrados} c: {cGrados} t: {t} z: {z} y: {y}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TrianguloII.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triángulos_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingreso de datos y conversiones
+            Console.WriteLine("Ingrese T: ");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese A: ");
+            double aGrados = double.Parse(Console.ReadLine());
+            double a = aGrados * ((Math.PI) / 180);
+
+            //Hallar datos restantes
+            double c = Math.PI - (Math.PI) / 2 - a;
+            double cGrados = c * 180 / Math.PI;
+            double z = (t * (Math.Sin(c))) / Math.Sin((Math.PI) / 2);
+            double tPow = Math.Pow(t, 2);
+            double zPow = Math.Pow(z, 2);
+            double y = Math.Sqrt(tPow - zPow);
+
+            //Print results
+            Console.Write($"Los valores restantes son \n z: {z} y: {y} c: {cGrados}");
+            Console.ReadLine();
+
+        }
+    }
+}

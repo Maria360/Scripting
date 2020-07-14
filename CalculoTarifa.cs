69b4db9ecf5631189caa6451d36d22f17e136e68
Maria360 <68033327+Maria360@users.noreply.github.com>
2020-07-14 12:10:03 -0500

    Add files via upload

---
+++ CalculoTarifa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoTarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+            double smmlv = salario / 980657;
+            if(smmlv < 2)
+            {
+                Console.Write("Su tarifa es A\n");
+            }
+            else if (2<=smmlv && smmlv<4)
+            {
+                Console.Write("Su tarifa es B\n");
+            }
+            else 
+            {
+                Console.Write("Su tarifa es C\n");
+            }
+
+            
+        }
+    }
+}
---
+++ CuotaModeradora.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoCuotaModeradora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+            double smmlv = salario / 980657;
+            if (smmlv < 2)
+            {
+                Console.Write("Tarifa: A\nCuota Moderadora: 3400\n");
+            }
+            else if (2 <= smmlv && smmlv < 5)
+            {
+                Console.Write("Tarifa: B\nCuota Moderadora: 13500\n");
+            }
+            else
+            {
+                Console.Write("Tarifa: C\nCuota Moderadora: 35600\n");
+            }
+        }
+    }
+}

2955d5bd3f83cd044f713e82fa3b0b99fbb23cf1
Maria360 <68033327+Maria360@users.noreply.github.com>
2020-07-14 10:59:41 -0500

    Add files via upload

---
+++ Ecuacion_Cuadratica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcuacionCuadratica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese los valores de\na:\nb:\nc:\n ");
+            double a = double.Parse(Console.ReadLine()), b = double.Parse(Console.ReadLine()), c = double.Parse(Console.ReadLine());
+            double discriminante = Math.Pow(b, 2) - 4 * a * c;
+            if (discriminante == 0)
+            {
+                double x = -b / 2 * a;
+                Console.Write("La solución es x: " + x);
+            }
+            else if (discriminante > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminante)) / 2 * a, x2 = (-b - Math.Sqrt(discriminante)) / 2 * a;
+                Console.Write($"La solución es\nx1: {x1}\nx2: {x2}\n");
+            }
+            else
+            {
+                Console.Write("No existe solución\n");
+            }
+        }
+    }
+}
---
+++ IMC.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese\nAltura en metros:\nPeso en Kg:\n ");
+            double talla = double.Parse(Console.ReadLine()), peso = double.Parse(Console.ReadLine());
+            double imc = peso / Math.Pow(talla,2);
+            if(imc < 18.5)
+            {
+                Console.Write($"Su IMC es {imc} y su clasificación es: peso insuficiente");
+            }
+            else if (18.5 <= imc && imc <= 24.9)
+            {
+                Console.Write($"Su IMC es {imc} y su clasificación es: peso normal");
+            }
+            else if(25<= imc && imc <= 29.9)
+            {
+                Console.Write($"Su IMC es {imc} y su clasificación es: sobrepeso");
+            }
+            else
+            {
+                Console.Write($"Su IMC es {imc} y su clasificación es: obesidad ");
+            }
+
+            Console.Write("\nFINITO\n");
+
+            /*Console.Write("Ingrese\nAltura en metros:\nPeso en Kg:\n ");
+            double talla = double.Parse(Console.ReadLine()), peso = double.Parse(Console.ReadLine());
+            double imc = peso / Math.Pow(talla, 2);
+
+            switch (imc)
+            {
+                case  imc<18.5:
+                Console.Write($"Su IMC es {imc} y su clasificación es: peso insuficiente");
+                break;
+            }*/
+
+
+        }
+    }
+}

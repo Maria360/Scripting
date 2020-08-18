7548068c7b864eac1f5ff3eb23b29c1c604df301
Maria360 <68033327+Maria360@users.noreply.github.com>
2020-08-18 10:17:47 -0500

    Add files via upload

---
+++ Euler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 100, x = 3, factorial=0, sumatoria=0;
+            for(int i = 0; i<=n; i++) 
+            {
+                factorial *= i;
+                if (factorial == 0) factorial = 1;
+                sumatoria += (Math.Pow(x, i))/(factorial);
+                
+            }
+            Console.WriteLine(sumatoria);
+        }
+    }
+}
---
+++ Sin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sin_x_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese x:");
+            double x = double.Parse(Console.ReadLine());
+            double sumatoria = 0;
+            int n = 100, divisor = 0;
+            double factorial = 0;
+            for (int i=0; i <= n; i++)
+            {
+                divisor = (2*i) + 1;
+                for(int j =1; j <=divisor; j++)
+                {
+                    factorial *= j;
+                    if (factorial == 0) factorial = 1;
+                }
+                
+                sumatoria += ((Math.Pow(-1, i)) / factorial) * Math.Pow(x, divisor);
+                
+            }
+            Console.WriteLine($"Sin({x})= "+sumatoria);
+        }
+    }
+}

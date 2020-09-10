5df13b0fa42ac559478b04eecb9acf17b7fffe10
Maria360 <68033327+Maria360@users.noreply.github.com>
2020-09-10 14:48:35 -0500

    Add files via upload

---
+++ Matrices D2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices_Desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string[] valores = { "-", "-", "o", "x" };
+            int n = 10;
+            int m = 15;
+            string[,] tablero = new string[n, m];
+
+
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    int indice = random.Next(0, valores.Length);
+                    tablero[i, j] = valores[indice];
+                }
+            }
+            Console.WriteLine("ENTRADA");
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    Console.Write("|" + tablero[i, j]);
+                }
+                Console.WriteLine("|\n");
+            }
+            Console.WriteLine("¿Donde desea colocar la X?\nfila: ");
+            int fila = int.Parse(Console.ReadLine());
+            Console.WriteLine("¿Donde desea colocar la X?\ncolumna:");
+            int columna = int.Parse(Console.ReadLine());
+            while(tablero[fila, columna] != "-")
+            {
+                Console.WriteLine($"La posición {fila},{columna} se encuentra ocupada\nSeleccione otra");
+                Console.WriteLine("¿Donde desea colocar la X?\nfila:");
+                fila = int.Parse(Console.ReadLine());
+                Console.WriteLine("¿Donde desea colocar la X?\ncolumna:");
+                columna = int.Parse(Console.ReadLine());
+            }
+            if (tablero[fila, columna] == "-") tablero[fila, columna] = "x";
+            Console.WriteLine("SALIDA");
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    Console.Write("|" + tablero[i, j]);
+                }
+                Console.WriteLine("|\n");
+            }
+        }
+    }
+}
---
+++ MatricesD1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string[] valores = { "-", "-", "o", "x" };
+            int n = 10;
+            int m = 15;
+            int x = 0, o = 0, guion = 0, total = 0;
+            double porcentajeX = 0, porcentajeO = 0, porcentajeGuion = 0;
+            string[,] tablero = new string[n, m];
+            string[,] salida = new string[n, m];
+
+            
+            for (int i=0; i < tablero.GetLength(0); i++)
+            {
+                for(int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    int indice = random.Next(0, valores.Length);
+                    tablero[i, j] = valores[indice];
+                    salida[i, j] = "-";
+                }
+            }
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    if (tablero[i, j] == "x") x++;
+                    if (tablero[i, j] == "o") o++;
+                    if (tablero[i, j] == "-") guion++;
+                }
+            }
+            total = x + o + guion;
+            porcentajeGuion = ((double)guion * 100) / total;
+            porcentajeX = ((double)x * 100) / total;
+            porcentajeO = ((double)o * 100) / total;
+            Console.WriteLine($"PORCENTAJES\nx: {porcentajeX}\no: {porcentajeO}\n-: {porcentajeGuion}");
+            Console.WriteLine("ENTRADA");
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    Console.Write("|"+tablero[i, j]);
+                }
+                Console.WriteLine("|\n");
+            }
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {//condiciones
+                for (int j = 0; j < tablero.GetLength(1)-2; j++)
+                {
+                    if (tablero[i, j] == "x") {
+                        if (tablero[i, j] == tablero[i, j + 1]&&tablero[i,j]==tablero[i,j+2]) {
+
+                            salida[i, j] = "1";
+                            salida[i, j + 1] = "1";
+                            salida[i, j + 2] = "1";
+                        }
+                    }
+                    if (tablero[i, j] == "o")
+                    {
+                        if (tablero[i, j] == tablero[i, j + 1] && tablero[i, j] == tablero[i, j + 2])
+                        {
+                            salida[i, j] = "2";
+                            salida[i, j + 1] = "2";
+                            salida[i, j + 2] = "2";
+                        }
+                    }
+
+                }
+            }
+            for (int i = 0; i < tablero.GetLength(0)-2; i++)
+            {//condiciones
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+
+                    if (tablero[i, j] == "x")
+                    {
+                        if (tablero[i, j] == tablero[i + 1, j] && tablero[i, j] == tablero[i + 2, j])
+                        {
+
+                            salida[i, j] = "1";
+                            salida[i+1, j] = "1";
+                            salida[i + 2, j ] = "1";
+                        }
+                    }
+                    if (tablero[i, j] == "o")
+                    {
+                        if (tablero[i, j] == tablero[i + 1, j] && tablero[i, j] == tablero[i + 2, j])
+                        {
+                            salida[i, j] = "2";
+                            salida[i + 1, j ] = "2";
+                            salida[i + 2, j ] = "2";
+                        }
+                    }
+                }
+            }
+            for (int i =0; i < salida.GetLength(0); i++)
+            {
+                for(int j=0; j < salida.GetLength(1) - 1; j++)
+                {
+
+                }
+            }
+            Console.WriteLine("SALIDA");
+            for (int i = 0; i < tablero.GetLength(0); i++)
+            {
+                for (int j = 0; j < tablero.GetLength(1); j++)
+                {
+                    Console.Write("|" + salida[i, j]);
+                }
+                Console.WriteLine("|\n");
+            }
+
+        }
+    }
+}
